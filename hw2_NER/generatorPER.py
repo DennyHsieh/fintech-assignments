87b6d913881cc08325e171009849d420f47b0066
DennyHsieh <tc.hsiehhsieh@gmail.com>
2019-04-07 17:31:07 +0800

    finish news crawler and translation

--- hw2_NER/generatorPER.py
+++ hw2_NER/generatorPER.py
@@ -1,38 +1,11 @@
 import os
-import requests
-from bs4 import BeautifulSoup
-from google import google
-from googletrans import Translator
 
-# Google search api
-num_page = 10
+per_data_folder = 'PER'
+if not os.path.exists(per_data_folder):
+    os.mkdir(per_data_folder)
 
-# Set the keywords and medias(蘋果、自由、中時)
-websites = [" site:appledaily.com.tw", " site:ltn.com.tw", " site:chinatimes.com"]
-contents = ["ndArticle_margin", "text", "article-body"]
-low_risk_keywords = ["人口販運", "性剝削、兒童", "偽造貨幣", "殺人、重傷害", "搶奪", "勒贖", "海盜", "恐怖主義、資恐"]
-medium_risk_keywords = ["非法販賣武器", "贓物", "竊盜", "綁架、拘禁等妨害自由", "環保犯罪", "偽造文書"]
-high_risk_keywords = ["仿冒、盜版、侵害營業秘密"]
-exhigh_risk_keywords = ["毒品販運", "詐欺", "走私", "稅務犯罪", "組織犯罪", "證券犯罪", "貪汙賄賂", "第三方洗錢"]
-all_keywords = list(low_risk_keywords + medium_risk_keywords + high_risk_keywords + exhigh_risk_keywords)
+os.chdir("./zh-NER-TF/")
 
-# Google translator
-trans = Translator()
+# TODO: put the result to zh-NER-TF model
+# os.system("python main.py --mode=demo --demo_model=1521112368")
 
-for keyword in all_keywords:
-    for cnt in range(len(websites)):
-        search_results = google.search(keyword + websites[cnt], num_page)
-        for result in search_results:
-            # Check the validity of link
-            if result.link is None:
-                continue
-
-            try:
-                res = requests.get(result.link, timeout=10).text
-            except:
-                # print('error page')
-                continue
-
-            soup = BeautifulSoup(res, 'html.parser')
-
-            # TODO: get the contents, and put the name result to zh-NER-TF model
---
+++ hw2_NER/news_crawler.py
@@ -0,0 +1,89 @@
+import os
+import requests
+import numpy as np
+from bs4 import BeautifulSoup
+from google import google
+from googletrans import Translator
+
+# Google search api
+num_page = 10
+
+# Set the keywords and news(蘋果、自由、中時)
+news = ["appledaily", "ltn", "chinatimes"]
+websites = [" site:appledaily.com.tw", " site:ltn.com.tw", " site:chinatimes.com"]
+contents = ["ndArticle_margin", "text", "article-body"]
+low_risk_keywords = ["人口販運", "性剝削", "兒童", "偽造貨幣", "殺人", "重傷害", "搶奪", "勒贖", "海盜", "恐怖主義", "資恐"]
+medium_risk_keywords = ["非法販賣武器", "贓物", "竊盜", "綁架", "拘禁", "妨害自由", "環保犯罪", "偽造文書"]
+high_risk_keywords = ["仿冒", "盜版", "侵害營業秘密"]
+exhigh_risk_keywords = ["毒品販運", "詐欺", "走私", "稅務犯罪", "組織犯罪", "證券犯罪", "貪汙賄賂", "第三方洗錢"]
+all_keywords = list(low_risk_keywords + medium_risk_keywords + high_risk_keywords + exhigh_risk_keywords)
+
+# Google translator
+trans = Translator()
+
+for keyword in all_keywords:
+    for cnt in range(len(websites)):
+        search_results = google.search(keyword + websites[cnt], num_page)
+
+        # Save searching results to numpy array
+        search_results_folder = 'search_results_npy'
+        if not os.path.exists(search_results_folder):
+            os.mkdir(search_results_folder)
+        fname_search = keyword + '_' + news[cnt] + '.npy'
+        fpath_search = os.path.join(search_results_folder, fname_search)
+        np.save(fpath_search, search_results)
+
+        links = []
+        for result in search_results:
+            # Check the validity of link
+            link = result.link
+            if link is None:
+                continue
+
+            try:
+                req = requests.get(link, timeout=10)
+                soup = BeautifulSoup(req.text, 'html.parser')
+            except:
+                # print('error page')
+                continue
+
+            title = result.name
+
+            cnts_p_tags = ''
+            find_cnts = soup.findAll('div', attrs={'class': contents[cnt]})
+            if find_cnts is None:
+                continue
+
+            for find_cnt in find_cnts:
+                if news[cnt] == 'appledaily':
+                    title = title.split("www")[0]
+                    find_p_tags = find_cnt.find('p')
+                    apple_text = find_p_tags.text
+                    ads = ['推薦新聞', '跟上國際脈動', '看了這則', '即時論壇徵稿']
+                    for ad in ads:
+                        try:
+                            apple_text = apple_text.split(ad)[0]
+                        except:
+                            continue
+                    cnts_p_tags += apple_text
+                else:
+                    title = title.split("http")[0]
+                    find_p_tags = find_cnt.findAll('p', attrs={'class': None})
+                    for find_p_tag in find_p_tags:
+                        if find_p_tag.string is None:
+                            continue
+                        cnts_p_tags += find_p_tag.text
+
+            # Translate to simplified Chinese
+            try:
+                cnts_p_tags_cn = trans.translate(cnts_p_tags, dest="zh-CN").text
+            except:
+                cnts_p_tags_cn = None
+                continue
+
+            print(news[cnt], title, result.link, cnts_p_tags, cnts_p_tags_cn)
+
+        # crawl_data_folder = 'crawl_data'
+        # if not os.path.exists(search_results_folder):
+        #     os.mkdir(search_results_folder)
+        # TODO: create csv
