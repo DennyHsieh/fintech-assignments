091bbb6bc0c3602d580597edae761b73b0f0d72d
DennyHsieh <tc.hsiehhsieh@gmail.com>
2019-06-21 12:46:59 +0800

    demo commit for final presentation

--- AML/data2web.py
+++ AML/data2web.py
@@ -9,22 +9,13 @@ output_csv = os.path.join(data_folder, 'final_demo_result.csv')
 df = pd.read_csv(data_csv).drop(['Unnamed: 0'], axis=1)
 # print(df.iloc[1])
 
-# df['age'] = None
-# df['title'] = None
-# df['profile_id'] = 0
 names = []
 
 # print(df['name'][6220:])
-def add_col(row):
-    for idx, val in enumerate(names):
-        if row['name'] not in names:
-            row['profile_id'] = idx
-        else:
-            row['profile_id'] = len(names)
-        return row
 
+# Drop len(name) = 1
+df = df[df['name'].apply(lambda x: len(x) > 1)]
 
-# df = df.apply(add_col, axis=1)
 # print(df.columns)
 new_df = df.reindex(
     columns=list(['name', 'age', 'title', 'crime', 'crime_risk', 'key_sentence', 'news_title', 'news_link',
--- final_demo/demo.py
+++ final_demo/demo.py
@@ -26,22 +26,22 @@ def result():
     title = request.form.get('title')
     valid_name_sql = 'SELECT * FROM potentialCrime '
 
-    if name == 'ALL':
-        cursor = db.execute(valid_name_sql)
-    else:
-        valid_name_sql += 'WHERE name = ?'
-        cursor = db.execute(valid_name_sql, (name,))
+    # if name == 'ALL':
+    #     cursor = db.execute(valid_name_sql)
+    # else:
+    valid_name_sql += 'WHERE name = ?'
+    cursor = db.execute(valid_name_sql, (name,))
 
+    if age != '':
+        valid_name_sql += 'AND age = ?'
+        cursor = db.execute(valid_name_sql, (name, age,))
+
+    if title != '':
+        valid_name_sql += 'AND title = ?'
         if age != '':
-            valid_name_sql += 'AND age = ?'
-            cursor = db.execute(valid_name_sql, (name, age,))
-
-        if title != '':
-            valid_name_sql += 'AND title = ?'
-            if age != '':
-                cursor = db.execute(valid_name_sql, (name, age, title,))
-            else:
-                cursor = db.execute(valid_name_sql, (name, title,))
+            cursor = db.execute(valid_name_sql, (name, age, title,))
+        else:
+            cursor = db.execute(valid_name_sql, (name, title,))
 
     valid_datas = []
     for row in cursor:
@@ -106,5 +106,5 @@ def close_connection(exception):
 
 
 if __name__ == '__main__':
-    # app.run()
-    app.run(debug=True)
+    app.run()
+    # app.run(debug=True)
